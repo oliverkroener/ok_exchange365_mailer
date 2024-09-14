ef292d323f6656aa744e3b0678eaafcb046e2dec
Oliver Kroener <ok@oliver-kroener.de>
2024-09-14 22:21:53 +0000

    [TASK] Changes for PHP 7

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,3 +1,17 @@
 <?php
 
 defined('TYPO3') || die();
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use OliverKroener\OkExchange365\Mail\Exchange365Mailer;
+use TYPO3\CMS\Core\Mail\MailerInterface;
+use TYPO3\CMS\Core\DependencyInjection\Container;
+
+// Register your custom mailer as the default mailer service
+call_user_func(function () {
+    $GLOBALS['TYPO3_CONF_VARS']['MAIL']['defaultMailer'] = Exchange365Mailer::class;
+
+    // Override the default mailer service with your custom implementation
+    //$container = GeneralUtility::makeInstance(Container::class);
+    //$container->registerImplementation(MailerInterface::class, CustomMailer::class);
+});
