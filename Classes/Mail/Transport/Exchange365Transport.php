bfaab1833e26a21dbf0655b576534e2d3180a580
Oliver Kroener <ok@oliver-kroener.de>
2024-09-15 19:16:09 +0000

    [TASK] Added backend .env configuration

--- Classes/Mail/Transport/Exchange365Transport.php
+++ Classes/Mail/Transport/Exchange365Transport.php
@@ -4,30 +4,26 @@ namespace OliverKroener\OkExchange365\Mail\Transport;
 
 use Exception;
 use Microsoft\Graph\Generated\Users\Item\SendMail\SendMailPostRequestBody;
-use Microsoft\Kiota\Abstractions\ApiException;
 use Microsoft\Kiota\Authentication\Oauth\ClientCredentialContext;
 use Microsoft\Graph\GraphServiceClient;
 use RuntimeException;
 use Symfony\Component\Mailer\Envelope;
 use Symfony\Component\Mailer\SentMessage;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use Symfony\Component\Mailer\Transport\TransportInterface;
 use Symfony\Component\Mime\RawMessage;
 use OliverKroener\Helpers\MSGraphApi\MSGraphMailApiService;
 use Psr\Log\LoggerInterface;
-use Psr\EventDispatcher\EventDispatcherInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Log\LogManager;
 
 class Exchange365Transport implements TransportInterface
 {
-    private $sentMessage;
-    private ?EventDispatcherInterface $dispatcher;
+    private array $mailSettings;
     private LoggerInterface $logger;
 
     public function  __construct(array $mailSettings)
     {
-        $this->dispatcher = GeneralUtility::makeInstance(EventDispatcherInterface::class);
+        $this->mailSettings = $mailSettings;
 
         // Initialize the logger using TYPO3's logging system
         $this->logger = GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__);
@@ -43,9 +39,20 @@ class Exchange365Transport implements TransportInterface
     public function send(RawMessage $message, ?Envelope $envelope = null): ?SentMessage
     {
         try {
-            $conf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_okexchange365mailer.']['settings.']['exchange365.'];
+            $conf = $GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_okexchange365mailer.']['settings.']['exchange365.'] ?? null;
 
-            $confFromEmail = $conf['fromEmail'];
+            // check if is not in frontend
+            if (!$conf) {
+                // get variables from globals
+                $conf = [];
+                $conf['tenantId'] = $this->mailSettings['transport_exchange365_tenantId'] ?? '';
+                $conf['clientId'] = $this->mailSettings['transport_exchange365_clientId'] ?? '';
+                $conf['clientSecret'] = $this->mailSettings['transport_exchange365_clientSecret'] ?? '';
+                $conf['fromEmail'] = $this->mailSettings['transport_exchange365_fromEmail'] ?? '';
+                $conf['saveToSentItems'] = $this->mailSettings['transport_exchange365_saveToSentItems'] ?? '';
+            }
+            $confFromEmail = $conf['fromEmail'] ?? '';
+            $saveToSentItems = $conf['saveToSentItems'] ?? 0;
 
             $tokenRequestContext = new ClientCredentialContext(
                 $conf['tenantId'],
@@ -60,13 +67,14 @@ class Exchange365Transport implements TransportInterface
 
             $requestBody = new SendMailPostRequestBody();
             $requestBody->setMessage($graphMessage);
+            $requestBody->setSaveToSentItems($saveToSentItems);
 
             // Send the email using Microsoft Graph API
             $graphServiceClient->users()->byUserId($confFromEmail)->sendMail()->post($requestBody)->wait();
 
         } catch (Exception $e) {
             $this->logger->alert('Sending mail from ' . $confFromEmail . ' failed!');
-            return null;
+            throw new RuntimeException("Sending mail with Exchange365 mailer failed. Please check credentials setup.");
         }
 
         $this->logger->debug('Mail sent successfully with ' . self::class);
@@ -78,36 +86,6 @@ class Exchange365Transport implements TransportInterface
         }
 
         return $sentMessage;
-    }
-
-    /**
-     * Gets the last sent message.
-     *
-     * @return SentMessage|null The sent message or null if none was sent.
-     */
-    public function getSentMessage(): ?SentMessage
-    {
-        return $this->sentMessage;
-    }
-
-    /**
-     * Gets the current transport interface.
-     *
-     * @return TransportInterface The transport interface in use.
-     */
-    public function getTransport(): TransportInterface
-    {
-        return $this->transport;
-    }
-
-    /**
-     * Gets the real transport interface.
-     *
-     * @return TransportInterface The real transport interface.
-     */
-    public function getRealTransport(): TransportInterface
-    {
-        return $this->transport; // Customize if a different transport is used
     }
 
     public function __toString(): string
