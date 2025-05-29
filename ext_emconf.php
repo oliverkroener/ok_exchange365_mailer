01a092a8f3e8acfb508abc63f3a0939092295b72
Oliver Kroener <ok@oliver-kroener.de>
2025-05-29 07:40:30 +0000

    [TASK] Removed V11 support and added documentation

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,11 +3,12 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Exchange 365 Mail Extension for OAuth2',
     'description' => 'A TYPO3 extension for sending emails using Exchange 365 and Microsoft Graph API',
+    'tags' => 'exchange365, mail, smtp, oauth2, microsoft graph',
     'category' => 'plugin',
     'author' => 'Oliver Kroener',
     'author_email' => 'ok@oliver-kroener.de',
     'state' => 'stable',
-    'version' => '4.0.1',
+    'version' => '4.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.0-13.99.99',
--- ext_localconf.php
+++ ext_localconf.php
@@ -2,9 +2,4 @@
 
 defined('TYPO3') || die();
 
-// TODO remove if support for TYPO3 11 dropped
-if ((\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class))->getMajorVersion() < 12 ) {
-    // Add Hook for blindedConfigurationValues
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][\TYPO3\CMS\Lowlevel\Controller\ConfigurationController::class]['modifyBlindedConfigurationOptions'][] =
-        OliverKroener\OkExchange365\Hook\BlindedConfigurationOptionsHook::class;
-}
+
